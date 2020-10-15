c47626edc09cc7a2edddd5867df0689743042448
nicholasmr <nicholas.rathmann@gmail.com>
2020-10-15 11:07:14 +0200

    details specfabpy.f90 comments and L40 as max. default.

--- gaunt.f90
+++ gaunt.f90
@@ -3,14 +3,13 @@
 module gaunt
 
     implicit none 
-
     integer, parameter, private :: dp = 8 ! Default precision
-    include "include/gaunt__head_L60.f90"        
-        
+    include "include/gaunt__head_L40.f90"        
+
 contains
 
 subroutine set_gaunts()
-    include "include/gaunt__body_L60.f90"
+    include "include/gaunt__body_L40.f90"
 end
     
 end module gaunt
