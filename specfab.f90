fa27b1644e80f040202d15f6d16b55831982dd04
nicholasmr <nicholas.rathmann@gmail.com>
2021-06-11 09:56:25 +0200

    added tensorial evolution for LATROT and REG

--- specfab.f90
+++ specfab.f90
@@ -393,8 +393,6 @@ subroutine ck_moments(nlm, ev_c2,ev_c4,ev_c6,ev_c8)
     ev_c8 = f_ev_c8(n00,n2m,n4m,n6m,n8m)
 end
       
-! Just some shortcuts for computational efficiency...
-      
 function a2_ij(nlm) 
     ! a^(2) := <c^2> 
     implicit none
@@ -416,7 +414,7 @@ function a4_ijkl(nlm)
     a4_ijkl = f_ev_c4(nlm(1),n2m,n4m)
 end
 
-! Reverse: tensorial expansion --> spectral expansion
+! tensorial expansion --> spectral expansion
 
 function a2_to_nlm(a2) result(nlm)
     ! Get n_2^m from a^(2)
@@ -437,8 +435,67 @@ function a4_to_nlm(a2, a4) result(nlm)
 end
 
 !---------------------------------
-! Dynamics in tensorial space
+! FABRIC DYNAMICS IN TENSORIAL SPACE
 !---------------------------------
+
+subroutine dndt_to_daidt(ddt_nlm, n00, da2dt, da4dt)
+
+    complex(kind=dp), intent(in) :: ddt_nlm(nlm_len), n00
+    real(kind=dp), intent(out)   :: da2dt(3,3), da4dt(3,3,3,3)
+    complex(kind=dp) :: ddt_n2m(-2:2), ddt_n4m(-4:4)
+    
+    ddt_n2m = ddt_nlm(I_l2:(I_l4-1))
+    ddt_n4m = ddt_nlm(I_l4:(I_l6-1))
+    
+    da2dt = f_ev_c2(n00, ddt_n2m)          - f_ev_c2(n00, 0*ddt_n2m)            ! <c^> entries are *linear* combinations of n_l^m, allowing d/dt <c^2> to be calculated by substituting "d/dt n_l^m" for "n_l^m" in "<c^2>(n_l^m)" by removing the constant part(s).
+    da4dt = f_ev_c4(n00, ddt_n2m, ddt_n4m) - f_ev_c4(n00, 0*ddt_n2m, 0*ddt_n4m) ! ...same reasoning as above
+end
+
+subroutine daidt_LATROT(eps, omg, a2, a4, da2dt, da4dt)
+    
+    ! Lattice rotation contribution to d/dt a^(2) and d/dt a^(4) 
+    ! Assumes Taylor hypothesis  (constant strain-rate over polycrystal).
+    
+    implicit none
+
+    real(kind=dp), intent(in)  :: eps(3,3), omg(3,3), a2(3,3), a4(3,3,3,3)
+    real(kind=dp), intent(out) :: da2dt(3,3), da4dt(3,3,3,3)
+    complex(kind=dp) :: nlm(nlm_len), ddt_nlm(nlm_len)
+    
+    nlm = a4_to_nlm(a2, a4) ! tensorial --> spectral, truncated at L=4
+    ddt_nlm = matmul(dndt_ij_LATROT(eps,omg, 0*eps,0d0,0d0,0d0, 1d0), nlm) ! spectral evolution  --- d/dt nlm_i = M_ij nlm_j 
+    call dndt_to_daidt(ddt_nlm, nlm(1), da2dt, da4dt) ! spectral --> tensorial 
+end
+
+subroutine daidt_DDRX(tau, Gamma0, a2, a4, da2dt, da4dt)
+    
+    ! DDRX contribution to d/dt a^(2) and d/dt a^(4) 
+    
+    implicit none
+
+    real(kind=dp), intent(in)  :: tau(3,3), Gamma0, a2(3,3), a4(3,3,3,3)
+    real(kind=dp), intent(out) :: da2dt(3,3), da4dt(3,3,3,3)
+    complex(kind=dp) :: nlm(nlm_len), ddt_nlm(nlm_len)
+    
+    nlm = a4_to_nlm(a2, a4) ! tensorial --> spectral, truncated at L=4
+    ddt_nlm = Gamma0 * matmul(dndt_ij_DDRX(nlm, tau), nlm) ! spectral evolution  --- d/dt nlm_i = M_ij nlm_j 
+    call dndt_to_daidt(ddt_nlm, nlm(1) , da2dt, da4dt) ! spectral --> tensorial 
+end
+
+subroutine daidt_REG(nu, a2, a4, da2dt, da4dt)
+    
+    ! Regularization contribution to d/dt a^(2) and d/dt a^(4) 
+    
+    implicit none
+
+    real(kind=dp), intent(in)  :: nu, a2(3,3), a4(3,3,3,3)
+    real(kind=dp), intent(out) :: da2dt(3,3), da4dt(3,3,3,3)
+    complex(kind=dp) :: nlm(nlm_len), ddt_nlm(nlm_len)
+    
+    nlm = a4_to_nlm(a2, a4) ! tensorial --> spectral, truncated at L=4
+    ddt_nlm = nu * matmul(dndt_ij_REG(), nlm) ! spectral evolution  --- d/dt nlm_i = M_ij nlm_j 
+    call dndt_to_daidt(ddt_nlm, nlm(1), da2dt, da4dt) ! spectral --> tensorial 
+end
 
 function a6_CBT(a2,a4)
 
@@ -455,36 +512,9 @@ function a6_CBT(a2,a4)
     n00 = nlm(1)
     n2m = nlm(I_l2:(I_l4-1))
     n4m = nlm(I_l4:(I_l6-1))
-    n6m = nlm(I_l6:(I_l8-1))
+    n6m = 0 
     
-    a6_CBT = f_ev_c6(n00, n2m, n4m, 0*n6m) ! Calculate a^(6) given the spectral truncation n_l^m = 0 for l>4
-    
-end
-
-subroutine daidt_DDRX(tau, Gamma0, a2, a4, da2dt, da4dt)
-    
-    ! DDRX contribution to d/dt a^(2) and d/dt a^(4) 
-    
-    implicit none
-
-    real(kind=dp), intent(in)  :: tau(3,3), Gamma0, a2(3,3), a4(3,3,3,3)
-    real(kind=dp), intent(out) :: da2dt(3,3), da4dt(3,3,3,3)
-    complex(kind=dp) :: nlm(nlm_len), ddt_nlm(nlm_len)
-    complex(kind=dp) :: n00, ddt_n2m(-2:2), ddt_n4m(-4:4)
-    
-    ! (1) tensorial --> spectral, truncated at L=4
-    nlm = a4_to_nlm(a2, a4)
-    n00 = nlm(1) ! = 1/sqrt(4*pi) due to ODF normalization 
-        
-    ! (2) Calculate spectral evolution due to DDRX
-    ddt_nlm = Gamma0 * matmul(dndt_ij_DDRX(nlm, tau), nlm) ! d/dt nlm_i = M_ij nlm_j 
-    ddt_n2m = ddt_nlm(I_l2:(I_l4-1))
-    ddt_n4m = ddt_nlm(I_l4:(I_l6-1))
-        
-    ! (3) spectral --> tensorial 
-    da2dt = f_ev_c2(n00, ddt_n2m)          - f_ev_c2(n00, 0*ddt_n2m)            ! <c^> entries are *linear* combinations of n_l^m, allowing d/dt <c^2> to be calculated by substituting "d/dt n_l^m" for "n_l^m" in "<c^2>(n_l^m)" by removing the constant part(s).
-    da4dt = f_ev_c4(n00, ddt_n2m, ddt_n4m) - f_ev_c4(n00, 0*ddt_n2m, 0*ddt_n4m) ! ...same reasoning as above
-
+    a6_CBT = f_ev_c6(n00, n2m, n4m, n6m) ! Calculate a^(6) given the spectral truncation n_l^m = 0 for l>4
 end
 
 !---------------------------------
